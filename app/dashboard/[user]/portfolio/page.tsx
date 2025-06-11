71ce4678643f5dda0a09c22ee034ed0b3aef8b77
Paul <mierlo.paul@hotmail.nl>
2025-06-11 18:29:18 +0200

    portfolio sketch

--- app/dashboard/[user]/portfolio/page.tsx
+++ app/dashboard/[user]/portfolio/page.tsx
@@ -9,22 +9,41 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 import { getUser } from "@/lib/getUser"
-
 import { Icons } from "@/components/icons/icons"
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs"
 
-export default async function PortfolioPage() {
+export default async function PortfolioPage({ amount }: { amount: Number }) {
   const { username } = await getUser()
   return (
     <Card className="w-full mx-auto mt-12">
-      <CardHeader className="items-center">
-        <Icons.DollarSign className="mb-2" />
-        {username}
-        <CardTitle></CardTitle>
-        <CardDescription></CardDescription>
-        <CardAction></CardAction>
-      </CardHeader>
-      <CardContent></CardContent>
-      <CardFooter className="flex-col gap-2"></CardFooter>
+      <Tabs defaultValue="SUSC" className="w-full">
+        <CardHeader className="items-center">
+          <div className="flex w-full items-center justify-between">
+            <h1 className="text-2xl font-bold">{username}</h1>
+            <TabsList>
+              <TabsTrigger value="SUSC">All Assets</TabsTrigger>
+              <TabsTrigger value="Other Assets">Select Asset</TabsTrigger>
+            </TabsList>
+          </div>
+          <div className="flex flex-row mt-2">
+            <Icons.DollarSign />
+            <p className="flex items-center text-lg font-bold text-muted-foreground text-black">
+              1.78
+            </p>
+          </div>
+          <CardDescription></CardDescription>
+          <CardAction></CardAction>
+        </CardHeader>
+        <CardContent>
+          <TabsContent value="SUSC">
+            <div className="py-4">Overview visualisation goes here.</div>
+          </TabsContent>
+          <TabsContent value="Other Assets">
+            <div className="py-4">Analytics visualisation goes here.</div>
+          </TabsContent>
+        </CardContent>
+        <CardFooter className="flex-col gap-2"></CardFooter>
+      </Tabs>
     </Card>
   )
 }
