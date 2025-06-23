5357ccfae9fe8108dd375b5a39e64f2295d9fd2d
Paul <mierlo.paul@hotmail.nl>
2025-06-23 21:27:10 +0200

    added swapinterface

--- app/dashboard/[user]/portfolio/page.tsx
+++ app/dashboard/[user]/portfolio/page.tsx
@@ -11,39 +11,45 @@ import {
 import { getUser } from "@/lib/getUser"
 import { Icons } from "@/components/icons/icons"
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs"
+import Swapinterface from "@/components/interfaces/Swapinterface"
 
 export default async function PortfolioPage({ amount }: { amount: Number }) {
   const { username } = await getUser()
   return (
-    <Card className="w-full mx-auto mt-12">
-      <Tabs defaultValue="SUSC" className="w-full">
-        <CardHeader className="items-center">
-          <div className="flex w-full items-center justify-between">
-            <h1 className="text-2xl font-bold">{username}</h1>
-            <TabsList>
-              <TabsTrigger value="SUSC">All Assets</TabsTrigger>
-              <TabsTrigger value="Other Assets">Select Asset</TabsTrigger>
-            </TabsList>
-          </div>
-          <div className="flex flex-row mt-2">
-            <Icons.DollarSign />
-            <p className="flex items-center text-lg font-bold text-muted-foreground text-black">
-              1.78
-            </p>
-          </div>
-          <CardDescription></CardDescription>
-          <CardAction></CardAction>
-        </CardHeader>
-        <CardContent>
-          <TabsContent value="SUSC">
-            <div className="py-4">Overview visualisation goes here.</div>
-          </TabsContent>
-          <TabsContent value="Other Assets">
-            <div className="py-4">Analytics visualisation goes here.</div>
-          </TabsContent>
-        </CardContent>
-        <CardFooter className="flex-col gap-2"></CardFooter>
-      </Tabs>
-    </Card>
+    <>
+      <Card className="w-full mx-auto mt-12">
+        <Tabs defaultValue="SUSC" className="w-full">
+          <CardHeader className="items-center">
+            <div className="flex w-full items-center justify-between">
+              <h1 className="text-2xl font-bold">{username}</h1>
+              <TabsList>
+                <TabsTrigger value="SUSC">All Assets</TabsTrigger>
+                <TabsTrigger value="Other Assets">Select Asset</TabsTrigger>
+              </TabsList>
+            </div>
+            <div className="flex flex-row mt-2">
+              <Icons.DollarSign />
+              <p className="flex items-center text-lg font-bold text-muted-foreground text-black">
+                1.78
+              </p>
+            </div>
+            <CardDescription></CardDescription>
+            <CardAction></CardAction>
+          </CardHeader>
+          <CardContent>
+            <TabsContent value="SUSC">
+              <div className="py-4">Overview visualisation goes here.</div>
+            </TabsContent>
+            <TabsContent value="Other Assets">
+              <div className="py-4">Analytics visualisation goes here.</div>
+            </TabsContent>
+          </CardContent>
+          <CardFooter className="flex-col gap-2"></CardFooter>
+        </Tabs>
+      </Card>
+      <div className="w-full max-w-2xl mx-auto mt-12">
+        <Swapinterface />
+      </div>
+    </>
   )
 }
